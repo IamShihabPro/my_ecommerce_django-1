ed2ed6c71816731181931f6e00f6079931c006c9
IamShihabPro <mshihab.pro@gmail.com>
2023-12-05 03:08:14 +0600

    product details

--- App_Shop/urls.py
+++ App_Shop/urls.py
@@ -5,5 +5,6 @@ from App_Shop import views
 
 
 urlpatterns = [
-   path('', views.Home.as_view(), name='home')
+   path('', views.Home.as_view(), name='home'),
+   path('product/<pk>/', views.ProductDetail.as_view(), name='product_detail'),
 ]
--- App_Shop/views.py
+++ App_Shop/views.py
@@ -6,6 +6,9 @@ from django.views.generic import ListView, DetailView
 # Models
 from App_Shop.models import Product
 
+# Mixin
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 # Create your views here.
 
 
@@ -13,3 +16,7 @@ class Home(ListView):
     model = Product
     template_name = 'App_Shop/home.html'
 
+
+class ProductDetail(LoginRequiredMixin, DetailView):
+    model = Product
+    template_name = 'App_Shop/product_detail.html'
